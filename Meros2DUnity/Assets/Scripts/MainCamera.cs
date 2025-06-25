7ced3c87dc38beb5d786bc146fba345b396ccd01
Denise <denise.vecino@gmail.com>
2025-06-25 10:35:41 -0300

    tentativa de corrigir botões

--- Meros2DUnity/Assets/Scripts/MainCamera.cs
+++ Meros2DUnity/Assets/Scripts/MainCamera.cs
@@ -7,12 +7,7 @@ public class MainCamera : MonoBehaviour
 
     public GameObject RigidBodyPlayer;
     private Vector3 offset = new Vector3(0, 0, -15);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-       
-    }
+
 
     // Update is called once per frame
     void Update()
